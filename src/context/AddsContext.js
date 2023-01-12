84e84b61b3b41afe609f090063645d1e4468c9c9
Kanykey Albanova <temirbekov91@gmail.com>
2023-01-12 14:50:04 -0500

    test-5

--- src/context/AddsContext.js
+++ src/context/AddsContext.js
@@ -40,10 +40,9 @@ const reducer = (state, action) => {
 export const AddsContext = createContext();
 export const AddsProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const url = "https://3621-66-212-242-25.ngrok.io";
   //this hook is used to get all data from mongoDB database
   useEffect(() => {
-    fetch(`${url}/adds`)
+    fetch("/api/adds")
       .then((res) => res.json())
       .then((data) => addAllAddsFn(data));
   }, [state.adds]);
@@ -55,7 +54,7 @@ export const AddsProvider = ({ children }) => {
   };
   //this function deletes an add based on id
   const deleteAnAdd = (_id) => {
-    fetch(`${url}/delete`, {
+    fetch("/api/delete", {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
@@ -70,7 +69,7 @@ export const AddsProvider = ({ children }) => {
       data,
     });
     if (state.editAdd) {
-      fetch(`${url}/update`, {
+      fetch("/api/update", {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
@@ -97,7 +96,7 @@ export const AddsProvider = ({ children }) => {
   //this function helps to create a new add and it adds it to database up on  submitting it
   const handleSubmitForm = (e) => {
     e.preventDefault();
-    fetch(`${url}/add/add`, {
+    fetch("/api/add/add", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
